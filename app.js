e4ec2b7ca7a3a049a497ddad85e320736dd1e99c
Andrew Storrow <andrew.storrow@gmail.com>
2018-01-02 18:04:20 -0600

    Add destroy route. Style index page. Sanitize input.

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express     = require("express"),
 app             = express(),
 bodyParser      = require("body-parser"),
 mongoose        = require("mongoose"),
-methodOverride  = require("method-override");
+methodOverride  = require("method-override"),
+expressSanitizer = require("express-sanitizer");
 
 //APP CONFIG
 mongoose.Promise = global.Promise;
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -23,7 +25,7 @@ var Blog = mongoose.model("Blog", blogSchema);
 
 // Blog.create({
 //     title: "Test Blog",
-//     image: "https://images.unsplash.com/photo-1494256997604-768d1f608cac?auto=format&fit=crop&w=500&q=80",
+//     image: "",
 //     body: "HELLO HELLO hello"
 // });
 
@@ -52,6 +54,7 @@ app.get("/blogs/new", function(req, res){
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
   //create blog
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err, newBlog){
       if(err){
           res.render("new");
@@ -86,6 +89,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 //UPDATE ROUTE
 app.put("/blogs/:id",function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.findByIdAndUpdate(req.params.id, req.body.blog, 
     function(err, updatedBlog){
       if(err){
@@ -94,6 +98,18 @@ app.put("/blogs/:id",function(req, res){
         res.redirect("/blogs/" + req.params.id);
       }
     });
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+	//destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.redirect("/blogs");
+		}
+	});
 });
 
 app.listen("3000", function(){
